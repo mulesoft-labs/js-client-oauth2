95331bdc52979a8d98fc7ed18b6aa4fa75c16808
Pierre Buyle <buyle@pheromone.ca>
2015-04-23 11:09:42 -0400

    Remove extra semicolons to pass linting test.

--- test/user.js
+++ test/user.js
@@ -105,11 +105,11 @@ describe('user', function () {
     it('should return false when token is not expired', function () {
       user.expiresIn(10)
       expect(user.expired()).to.be.equal(false)
-    });
+    })
 
     it('should return true when token is expired', function () {
       user.expiresIn(-10)
       expect(user.expired()).to.be.equal(true)
-    });
-  });
+    })
+  })
 })
