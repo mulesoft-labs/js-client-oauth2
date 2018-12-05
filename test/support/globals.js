59c604d98af22b231525f74b3741225b3f4610b8
Artem Kostiuk <postatum@gmail.com>
2018-12-05 19:06:54 +0200

    Fix standard notes

--- test/support/globals.js
+++ test/support/globals.js
@@ -4,5 +4,5 @@ Object.assign = Object.assign || require('object-assign')
 global.Promise = global.Promise || require('es6-promise').Promise
 
 global.btoa = global.btoa || function (str) {
-  return new Buffer(str).toString('base64')
+  return Buffer.from(str).toString('base64')
 }
